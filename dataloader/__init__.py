a196488facbe9ce0a4f2ce0038f273e62df3d2c0
Ean <45066060+Nam-SW@users.noreply.github.com>
2021-01-13 14:35:46 +0900

    feat: 데이터 전처리 방식 수정

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,135 +1,170 @@
-from os.path import abspath
+from os.path import abspath, join
+from typing import Optional
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-
-from .preprocessing import load_data
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 class DataLoader:
     def __init__(
         self,
-        contexts: np.ndarray,
-        response: np.ndarray,
-        Y: np.ndarray,
+        data: str,
+        contexts_max_len: int,
+        response_max_len: int,
         shuffle=True,
         mask_ids=4,
     ):
-        self.contexts = contexts
-        self.response = response
-        self.Y = Y
+        self.data = data
+        self.contexts_max_len = contexts_max_len
+        self.response_max_len = response_max_len
         self.shuffle = shuffle
         self.mask_ids = mask_ids
+        self.cls_token = self.str_to_list(self.data.iloc[0, 0])[0]
 
-        self.utterance_size = self.contexts.shape[1]
-
-    def start_of_epoch(self):
-        if self.shuffle:
-            s = np.arange(len(self.Y))
-            np.random.shuffle(s)
-
-            self.contexts = self.contexts[s]
-            self.response = self.response[s]
-            self.Y = self.Y[s]
-
-    def get_wor_data(self, batch):
-        shuffle_idx = np.random.choice([*range(self.utterance_size)])
-
-        x = batch[shuffle_idx].copy()
-        y = batch[shuffle_idx].copy()
-
-        length = np.count_nonzero(x)
-        ids = x[:length]
-        np.random.shuffle(ids)
-        x[:length] = ids
-
-        return x.tolist(), y.tolist()
-
-    def get_uor_data(self, batch):
-        # TODO: implement
-        return [0], [0]
-
-    def get_mwr_data(self, batch):
-        x = batch.copy()
-        for i in range(self.utterance_size):
-            length = np.count_nonzero(x[i])
-            mask_count = np.ceil(length * 0.15).astype(np.int32)
-            mask_w_idx = np.random.choice(np.arange(length), mask_count)
-            x[i, mask_w_idx] = self.mask_ids
-        y = batch.copy()
-
-        return x.tolist(), y.tolist()
-
-    def get_mur_data(self, batch):
-        x = batch.copy()
-        idx = np.random.choice([*range(self.utterance_size)])
-        x[idx] = self.mask_ids
-        y = batch.copy()
-
-        return x.tolist(), y.tolist()
-
-    def MLE_Task(self):
-        for i in range(len(self)):
-            # idx = [*range(i * self.batch_size, (i + 1) * self.batch_size)]
-            # contexts = self.contexts[idx]
-            # response = self.response[idx]
-            # Y = self.Y[idx]
-            contexts = self.contexts[i]
-            response = self.response[i]
-            Y = self.Y[i]
-
-            yield (contexts.tolist(), response.tolist()), Y
-
-    def Auxiliary_Task(self):
-        for i in range(len(self)):
-            # idx = [*range(i * self.batch_size, (i + 1) * self.batch_size)]
-            # contexts = self.contexts[idx]
-            contexts = self.contexts[i]
-
-            wor = self.get_wor_data(contexts)
-            uor = self.get_uor_data(contexts)
-            mwr = self.get_mwr_data(contexts)
-            mur = self.get_mur_data(contexts)
-
-            tasks = (wor, uor, mwr, mur)
-
-            yield tasks
-
-    def load_data(self):
-        self.start_of_epoch()
-
-        for mle, auxiliary in zip(self.MLE_Task(), self.Auxiliary_Task()):
-            yield mle, auxiliary
+        self.utterance_size = self.data.shape[1] - 2
+        self.on_epoch_end()
 
     def __len__(self):
-        return len(self.Y)
+        return len(self.data)
+
+    def str_to_list(self, s):
+        return list(map(int, s[1:-1].split(", ")))
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            self.data = self.data.sample(frac=1).reset_index(drop=True)
+
+    def padding(self, x, length):
+        return pad_sequences([x], length, padding="post", dtype=np.int32)[0]
+
+    def add_cls(self, x):
+        return [self.cls_token] + x
+
+    def MLE_Task(self, row):
+        context = self.add_cls(sum(row[: self.utterance_size], []))
+        context = self.padding(context, self.contexts_max_len)
+        response = self.padding(
+            row[self.utterance_size], self.response_max_len - 1
+        )
+        y = self.padding(row[-1], self.response_max_len - 1)
+
+        return {"context_ids": context, "response_ids": response, "y": y}
+
+    def WOR_Task(self, row):
+        shuffle_idx = np.random.choice([*range(self.utterance_size)])
+        x = row[shuffle_idx]
+        y = self.padding(self.add_cls(row[shuffle_idx]), self.response_max_len)
+
+        np.random.shuffle(x)
+        x = self.padding(self.add_cls(x), self.response_max_len)
+
+        return {"context_ids": x, "y": y}
+
+    def UOR_Task(self, row):
+        # TODO: implement
+        temp = np.zeros(self.contexts_max_len)
+        return {"context_ids": temp, "y": temp}
+
+    def MWR_Task(self, row):
+        x = row.copy()
+        for i in range(self.utterance_size):
+            c = np.array(x[i])
+            length = len(c)
+            mask_count = np.ceil(length * 0.15).astype(np.int32)
+            mask_w_idx = np.random.choice(np.arange(length), mask_count)
+            c[mask_w_idx] = self.mask_ids
+            x[i] = c.tolist()
+
+        x = self.padding(
+            self.add_cls(sum(x[: self.utterance_size], [])),
+            self.contexts_max_len,
+        )
+        y = self.padding(
+            self.add_cls(sum(row[: self.utterance_size], [])),
+            self.contexts_max_len,
+        )
+
+        return {"context_ids": x, "y": y}
+
+    def MUR_Task(self, row):
+        x = row.copy()
+        idx = np.random.choice([*range(self.utterance_size)])
+        x[idx] = [self.mask_ids] * len(x[idx])
+
+        x = self.padding(
+            self.add_cls(sum(x[: self.utterance_size], [])),
+            self.contexts_max_len,
+        )
+        y = self.padding(
+            self.add_cls(sum(row[: self.utterance_size], [])),
+            self.contexts_max_len,
+        )
+
+        return {"context_ids": x, "y": y}
+
+    def __getitem__(self, i):
+        row = self.data.iloc[i].apply(self.str_to_list)
+        for i in range(self.utterance_size):
+            row[i] = row[i][1:]
+
+        data = dict()
+        data["MLE"] = self.MLE_Task(row)
+        data["WOR"] = self.WOR_Task(row)
+        data["UOR"] = self.UOR_Task(row)
+        data["MWR"] = self.MWR_Task(row)
+        data["MUR"] = self.MUR_Task(row)
+
+        return data
+
+    def __call__(self):
+        for i in range(len(self)):
+            yield self.__getitem__(i)
+
+        self.on_epoch_end()
 
 
 def get_dataloader(
-    data_dir: str,
-    validation_split: float,
-    # batch_size=32,
-    shuffle=True,
-    mask_ids=4,
+    data_path: str,
+    contexts_max_len: int = 128,
+    response_max_len: int = 64,
+    validation_split: int = 0.1,
+    shuffle: bool = True,
+    mask_ids: int = 4,
 ):
-    data_dir = abspath(data_dir)
-    train, test = load_data(data_dir, validation_split)
+    data_path = abspath(data_path)
+    data = pd.read_csv(data_path, encoding="utf-8")
+    if shuffle:
+        data = data.sample(frac=1).reset_index(drop=True)
+    std_idx = int(len(data) * validation_split)
+    train_data = data.iloc[:-std_idx]
+    test_data = data.iloc[-std_idx:]
 
-    train_dataloader = DataLoader(*train, shuffle, mask_ids)
-    test_dataloader = DataLoader(*test, shuffle, mask_ids)
+    train_dataloader = DataLoader(
+        train_data, contexts_max_len, response_max_len, shuffle, mask_ids
+    )
+    test_dataloader = DataLoader(
+        test_data, contexts_max_len, response_max_len, shuffle, mask_ids
+    )
 
     return train_dataloader, test_dataloader
 
 
 def get_tf_data(dataloader: DataLoader, global_batch_size, repeat=True):
-    dtypes = (
-        ((tf.int32, tf.int32), tf.int32),
-        tuple([(tf.int32, tf.int32) for _ in range(4)]),
-    )
+    dtypes = {
+        "MLE": {
+            "context_ids": tf.int32,
+            "response_ids": tf.int32,
+            "y": tf.int32,
+        }
+    }
+    for key in ["WOR", "UOR", "MWR", "MUR"]:
+        dtypes[key] = {"context_ids": tf.int32, "y": tf.int32}
     dataset = tf.data.Dataset.from_generator(dataloader, output_types=dtypes)
 
     if repeat:
         dataset = dataset.repeat()
     dataset = dataset.batch(global_batch_size)
 
-    return dataset
+    return dataset.prefetch(tf.data.experimental.AUTOTUNE)
--- dataloader/preprocessing.py
+++ dataloader/preprocessing.py
@@ -2,10 +2,7 @@ import os
 from multiprocessing import Pool, cpu_count
 from typing import List, Tuple
 
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tqdm import tqdm
 
 from utils.filtering import filtering
@@ -26,9 +23,11 @@ def preprocessing(df: pd.DataFrame, remove_names: List[str], tokenizer):
     df["contents"] = df["contents"].str.replace(r"^이모티콘 (?=\w+)", "")
     df["contents"] = df["contents"].apply(filtering)
     df["contents"] = df["contents"].str.replace(remove_names, "[NAME]")
-    df = df[df["contents"].str.len() >= 5]
+    df = df[df["contents"].str.len() >= 2]
     df.reset_index(inplace=True, drop=True)
-    df["ids"] = df["contents"].apply(lambda x: tokenizer.encode(x).ids)
+    df["ids"] = list(
+        map(lambda x: x.ids, tokenizer.encode_batch(df["contents"].to_list()))
+    )
 
     return df
 
@@ -36,6 +35,14 @@ def preprocessing(df: pd.DataFrame, remove_names: List[str], tokenizer):
 def save_data(params: Tuple[pd.DataFrame, int, str]):
     assert len(params) == 3, "params must be 3."
     df, utterance_size, filename = params
+
+    if filename and os.path.isfile(filename):
+        df = pd.read_csv(filename, encoding="utf-8")
+        for column in df.columns[:-1]:
+            df[column] = df[column].apply(
+                lambda s: list(map(int, s[1:-1].split(", ")))
+            )
+        return df
 
     data = pd.DataFrame(
         columns=[f"utterance_{i+1}" for i in range(utterance_size)]
@@ -47,9 +54,12 @@ def save_data(params: Tuple[pd.DataFrame, int, str]):
         "content": initial_content.copy(),
         "last_original": "",
         "writer": None,
-        "utterances": [initial_content.copy() for _ in range(utterance_size + 1)],
+        "utterances": [
+            initial_content.copy() for _ in range(utterance_size + 1)
+        ],
     }
-    for i in tqdm(range(len(df))):
+    # for i in tqdm(range(len(df))):
+    for i in range(len(df)):
         row = df.iloc[i]
         if (
             temp_dict["writer"] != row["writer"]
@@ -74,7 +84,9 @@ def save_data(params: Tuple[pd.DataFrame, int, str]):
     temp_dict["utterances"].append(temp_dict["content"])
     temp_dict["utterances"] = temp_dict["utterances"][1:]
     line = temp_dict["utterances"] + [temp_dict["last_original"]]
-    data = data.append(pd.Series(line, index=data.columns), ignore_index=True)  # 추가
+    data = data.append(
+        pd.Series(line, index=data.columns), ignore_index=True
+    )  # 추가
 
     if filename:
         data.to_csv(filename, encoding="utf-8", index=None)
@@ -96,7 +108,8 @@ def get_preprocessed_data(
         if f.endswith(".csv")
     ]
     df_list = [
-        preprocessing(pd.read_csv(f), remove_names, tokenizer) for f in raw_df_names
+        preprocessing(pd.read_csv(f), remove_names, tokenizer)
+        for f in raw_df_names
     ]
 
     preprocess_df_names = [
@@ -125,19 +138,19 @@ def get_preprocessed_data(
 def _data_func(param):
     data, utterance_size = param
     new_data = pd.DataFrame(
-        columns=[f"context_{i+1}" for i in range(utterance_size)] + ["response", "y"]
+        columns=[f"context_{i+1}" for i in range(utterance_size)]
+        + ["response", "y"]
     )
 
-    for i in range(len(data)):
+    for i in tqdm(range(len(data))):
         row = data.iloc[i]
         contexts = row[:4].to_list()
-        for j in range(1, len(row[4])):
-            response = row[4][:j]
-            y = row[4][j]
-            line = contexts + [response, y]
-            new_data = new_data.append(
-                pd.Series(line, index=new_data.columns), ignore_index=True
-            )
+        response = row[4][:-1]
+        y = row[4][1:]
+        line = contexts + [response, y]
+        new_data = new_data.append(
+            pd.Series(line, index=new_data.columns), ignore_index=True
+        )
 
     return new_data
 
@@ -150,6 +163,8 @@ def get_data(
     use_multiprocessing=True,
 ):
     fname = os.path.join(model_use_data_dir, "data.csv")
+    if os.path.isfile(fname):
+        return pd.read_csv(fname, encoding="utf-8")
 
     data = pd.concat(data_list)
     data.reset_index(inplace=True, drop=True)
@@ -174,42 +189,7 @@ def get_data(
     else:
         new_data = _data_func((data, utterance_size))
 
-    new_data.to_csv(fname, encoding="utf-8", index=None)
-
-    context = np.transpose(
-        [
-            pad_sequences(
-                # new_data[col].to_list(), max_len, truncating="post", padding="post"
-                new_data[col].to_list(),
-                max_len,
-            )
-            for col in new_data.columns[:utterance_size]
-        ],
-        axes=[1, 0, 2],
-    )
-    response = pad_sequences(
-        # new_data["response"].to_list(), max_len, truncating="post", padding="post"
-        new_data["response"].to_list(),
-        max_len,
-    )
-    # y = pad_sequences(
-    #     new_data["y"].to_list(), max_len - 1, truncating="post", padding="post"
-    # )
-    y = new_data["y"].to_numpy()
-
-    return context, response, y
-
-
-def _save_whole_data(model_use_data_dir: str, context, response, y):
-    context_fname = os.path.join(model_use_data_dir, "context.npy")
-    response_fname = os.path.join(model_use_data_dir, "response.npy")
-    y_fname = os.path.join(model_use_data_dir, "y.npy")
-
-    np.save(context_fname, context)
-    np.save(response_fname, response)
-    np.save(y_fname, y)
-
-    print("save model use data at " + model_use_data_dir)
+    return new_data
 
 
 def setup_data(
@@ -243,51 +223,33 @@ def setup_data(
         tokenizer,
         use_multiprocessing,
     )
+    print(type(data_list[0].iloc[0, 0]))
 
     print("data setting")
-    context, response, y = get_data(
+    data = get_data(
         data_list,
         utterance_size,
         max_len,
         model_use_data_dir,
         use_multiprocessing,
     )
-
-    _save_whole_data(model_use_data_dir, context, response, y)
-
-
-def load_data(data_dir: str, validation_split: float):
-    data_dir = os.path.abspath(data_dir)
-
-    context_fname = os.path.join(data_dir, "context.npy")
-    response_fname = os.path.join(data_dir, "response.npy")
-    y_fname = os.path.join(data_dir, "y.npy")
-
-    csv_fname = os.path.join(data_dir, "data.csv")
-
-    if (
-        os.path.isfile(context_fname)
-        and os.path.isfile(response_fname)
-        and os.path.isfile(y_fname)
-    ):
-        context = np.load(context_fname)
-        response = np.load(response_fname)
-        y = np.load(y_fname)
-
-    elif os.path.isfile(csv_fname):
-        new_data = pd.read_csv(csv_fname, encoding="utf-8")
-        for column in new_data.columns:
-            new_data[column] = new_data[column].apply(
-                lambda x: list(map(int, x[1:-1].split(", ")))
-            )
-
-    else:
-        raise Exception("Data file Not found.")
-
-    con_train, con_test, y_train, y_test = train_test_split(
-        context, y, test_size=0.1, random_state=1
+    data.to_csv(
+        os.path.join(model_use_data_dir, "data.csv"),
+        encoding="utf-8",
+        index=None,
     )
-    res_train, res_test, y_train, y_test = train_test_split(
-        response, y, test_size=0.1, random_state=1
-    )
-    return (con_train, res_train, y_train), (con_test, res_test, y_test)
+    # data.sample(frac=1).reset_index(drop=True)
+
+    # i = int(len(data) * train_test_split)
+    # train_data = data.iloc[:-i]
+    # valid_data = data.iloc[-i:]
+    # train_data.to_csv(
+    #     os.path.join(model_use_data_dir, "train_data.csv"),
+    #     encoding="utf-8",
+    #     index=None,
+    # )
+    # valid_data.to_csv(
+    #     os.path.join(model_use_data_dir, "valid_data.csv"),
+    #     encoding="utf-8",
+    #     index=None,
+    # )
