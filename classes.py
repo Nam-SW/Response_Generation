3232b77c07916cde1e4422479e60d94d99d2a694
Ean <45066060+Nam-SW@users.noreply.github.com>
2021-04-29 01:04:27 +0900

    feat: 캐시 데이터 관리자 추가

--- classes.py
+++ classes.py
@@ -1,18 +1,33 @@
 from typing import List
 
+from utils import filtering
 
-class MassageCash:
-    def __init__(self):
-        self._massage_queue = dict()
+
+class CashMannager:
+    def __init__(self, tokenizer):
+        self.bos = tokenizer.bos_token
+        self.eos = tokenizer.eos_token
+        self.sep = tokenizer.sep_token
+
+        self._massage_logs = dict()
         self._latest_request_time_dict = dict()
-        
+
     def get_messages(self, channel_id) -> List[str]:
-        pass
+        return self._massage_logs.get(
+            channel_id, {"messages": [], "last_massage_id": None}
+        )
 
-    def add_message(self, channel_id, author, message):
-        pass
+    def add_message(self, message):
+        text = filtering(message.content)
+        author_id = message.author.id
+        channel_id = message.channel.id
 
-    def check_time(self, channel_id) -> bool:
+        message_dict = self.get_messages(channel_id)
+
+        if author_id == message_dict["last_massage_id"]:
+            pass
+
+    def check_last_one(self, massage) -> bool:
         pass
 
     def clear_cash(self, channel_id):
