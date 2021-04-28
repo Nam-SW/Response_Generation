61f197456cda1eef1899ab45f36bf7b727ee31a5
Ean <45066060+Nam-SW@users.noreply.github.com>
2021-04-29 01:03:36 +0900

    fit: 파일 구조 수정 및 필터링 추가

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import json
 import os
+import re
+
+from soynlp import repeat_normalize
 
 
 class JsonManager:
@@ -35,3 +38,31 @@ def load_jsonl(input_path) -> list:
         for line in f:
             data.append(json.loads(line.rstrip("\n|\r")))
     return data
+
+
+def discord_filtering():
+    pass
+
+
+def filtering(text):
+    def check_typo(text):
+        if re.match("[ㄱ-ㅎ]+", text):
+            text_set = set(text)
+            if len({"ㅋ", "ㅌ", "ㄴ"} - text_set) < 2:
+                text = "ㅋ" * len(text)
+
+            elif {"ㅎ", "ㄹ"} == text_set:
+                text = "ㅎ" * len(text)
+
+        return text
+
+    text = check_typo(text.strip().lower())
+    text = re.sub(r"사진 \d+장", "사진", text)
+    text = re.sub(r" {2,}", " ", text)
+    text = re.sub(
+        r"[^ .,?!%~\^\[\]\-_가-힣ㄱ-ㅎㅏ-ㅣa-z0-9]+|http.+|(?<=\d),(?=\d)", "", text
+    )
+
+    text = repeat_normalize(text, 6)
+    text = re.sub(r"(.{6,}?)\1+", r"\1", text)
+    return text
