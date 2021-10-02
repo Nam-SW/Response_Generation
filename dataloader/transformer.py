3a9511a61a5df18ae89574bd98b5ea3a3488b914
Ean <45066060+Nam-SW@users.noreply.github.com>
2021-10-03 02:17:38 +0900

    feat: 모델 구조 변경에 따른 dataloader 수정

--- dataloader/transformer.py
+++ dataloader/transformer.py
@@ -1,8 +1,10 @@
 from os.path import abspath, splitext
 from typing import Optional
 
+import numpy as np
 import tensorflow as tf
 from datasets import load_dataset, logging
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 logging.set_verbosity(logging.ERROR)
 
@@ -10,6 +12,7 @@ logging.set_verbosity(logging.ERROR)
 def load(
     tokenizer,
     seq_len: int,
+    window: int,
     train_data_path: str,
     eval_data_path: Optional[str] = None,
     train_test_split: Optional[float] = None,
@@ -17,45 +20,71 @@ def load(
     batch_size: int = 1000,
     shuffle_seed: Optional[int] = None,
 ):
-    def _split_decoder_data(sample):
-        sample["encoder_input"] = sample["encoder_input"].replace("<sep>", " ")
-        sample["decoder_input"] = sample["decoder_input"].replace("<sep>", " ")
-        sample["labels"] = sample["decoder_input"] + tokenizer.eos_token
-        sample["decoder_input"] = tokenizer.bos_token + sample["decoder_input"]
+    def _tokenize(sample):
+        sample["tokenized"] = tokenizer(sample["content"])["input_ids"]
         return sample
 
-    def _tokenize_function(sample):
-        tokenized = dict()
+    def _grouping(sample):
+        input_ids = []
+        labels = []
 
-        e = tokenizer(
-            sample["encoder_input"],
-            max_length=seq_len,
-            padding="max_length",
-            truncation=True,
-            return_tensors="np",
-        )
-        tokenized["input_ids"] = e["input_ids"]
-        tokenized["attention_mask"] = e["attention_mask"]
+        contents = [[] for _ in range(window - 1)] + sample["tokenized"]
+        talk_ids = [sample["talk_id"][0] for _ in range(window - 1)] + sample["talk_id"]
 
-        d = tokenizer(
-            sample["decoder_input"],
-            max_length=seq_len,
-            padding="max_length",
-            truncation=True,
-            return_tensors="np",
-        )
-        tokenized["decoder_input_ids"] = d["input_ids"]
-        tokenized["decoder_padding_mask"] = e["attention_mask"]
+        s, e = 0, window
+        now_talk_id = talk_ids[0]
 
-        tokenized["labels"] = tokenizer(
-            sample["labels"],
-            max_length=seq_len,
-            padding="max_length",
-            truncation=True,
-            return_tensors="np",
-        )["input_ids"]
+        while len(contents) > e:
+            talk_id = talk_ids[e]
 
-        return tokenized
+            if now_talk_id != talk_id:
+                contents = [[] for _ in range(window - 1)] + contents[s + window :]
+                talk_ids = [talk_id for _ in range(window - 1)] + talk_ids[s + window :]
+                s, e = 0, window
+                now_talk_id = talk_id
+                continue
+
+            input_ids.append(contents[s:e])
+            labels.append(contents[e])
+
+            s += 1
+            e += 1
+
+        return {"input_ids": input_ids, "labels": labels}
+
+    def padding_and_set_attention_mask(sample):
+        def _padding(data):
+            return pad_sequences(
+                data,
+                seq_len,
+                padding="post",
+                truncating="post",
+            )
+
+        input_ids = sum(sample["input_ids"], [])
+        input_ids = np.reshape(_padding(input_ids), (-1, window, seq_len))
+        attention_mask = (input_ids != 0).astype(np.int32)
+
+        decoder_input_ids = [[tokenizer.bos_token_id] + s for s in sample["labels"]]
+        decoder_input_ids = _padding(decoder_input_ids)
+        decoder_attention_mask = (decoder_input_ids != 0).astype(np.int32)
+
+        labels = [s + [tokenizer.eos_token_id] for s in sample["labels"]]
+        labels = _padding(labels)
+
+        # print(input_ids.shape)
+        # print(attention_mask.shape)
+        # print(decoder_input_ids.shape)
+        # print(decoder_attention_mask.shape)
+        # print(labels.shape)
+
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "decoder_input_ids": decoder_input_ids,
+            "decoder_attention_mask": decoder_attention_mask,
+            "labels": labels,
+        }
 
     train_data_path = abspath(train_data_path)
     is_eval = False
@@ -83,33 +112,43 @@ def load(
     data = load_dataset(
         extention.replace(".", ""), data_files=datafiles, split=train_test_split
     )
-    if shuffle_seed:
-        data = data.shuffle(seed=shuffle_seed)
 
     data = data.map(
-        _split_decoder_data,
+        _tokenize,
+        batched=True,
+        batch_size=batch_size,
         num_proc=worker,
     )
 
     data = data.map(
-        _tokenize_function,
+        _grouping,
         batched=True,
         batch_size=batch_size,
         num_proc=worker,
         remove_columns=data["train"].column_names,
     )
 
+    data = data.map(
+        padding_and_set_attention_mask,
+        batched=True,
+        batch_size=batch_size,
+        num_proc=worker,
+    )
+
+    if shuffle_seed:
+        data = data.shuffle(seed=shuffle_seed)
+
     return data["train"], (data["test"] if is_eval else None)
 
 
 def create_look_ahead_mask_collator(data):
-    padding_mask = 1 - tf.cast(tf.equal(data["decoder_input_ids"], 0), tf.int64)
+    padding_mask = 1 - tf.cast(tf.equal(data["decoder_input_ids"], 0), tf.int32)
     size = tf.shape(padding_mask)[-1]
 
     look_ahead_mask = tf.linalg.band_part(tf.ones((size, size)), -1, 0)
 
     data["decoder_look_ahead_mask"] = tf.minimum(
         padding_mask[:, tf.newaxis, tf.newaxis, :],
-        tf.cast(look_ahead_mask, tf.int64),
+        tf.cast(look_ahead_mask, tf.int32),
     )
     return data
